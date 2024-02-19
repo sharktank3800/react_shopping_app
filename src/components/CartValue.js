23c574e03723b55be2aeb0d0880b40b71852d3ac
[Ayman Shiham] <sharktank38000@gmail.com>
2024-02-19 18:25:14 -0500

    complete CartValue component

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -1,7 +1,18 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
+// Import the useContext hook, and pass AppContext to it - this is how a component connects to the context in order to get values from global state.
 const CartValue = () => {
+    const{expenses, Location} = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.unitprice * item.quantity));
+    }, 0);
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Cart Value: {Location}{totalExpenses}</span>
+        </div>
+    )
 };
 
 export default CartValue;
